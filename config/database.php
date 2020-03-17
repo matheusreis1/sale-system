0ced641d6f6ab5cd77646f712f10f8515400a9eb
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-03-17 18:30:33 -0300

    Added database connection config

---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+
+class Database {
+
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+        } catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
