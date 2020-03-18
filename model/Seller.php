c8923d1d47df9267b56a9d84eb1f945edc81398f
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-03-18 13:16:30 -0300

    Seller class added

---
+++ model/Seller.php
@@ -0,0 +1,27 @@
+<?php
+
+class Seller {
+
+    private $id;
+    private $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+    
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+}
