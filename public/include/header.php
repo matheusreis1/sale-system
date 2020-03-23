452e9f3c01c816c312f4b02c43bb2f44cda98fd4
Matheus Reis <theus.h.ass.reis@gmail.com>
2020-03-23 11:22:41 -0300

    Changes in navbar

--- public/include/header.php
+++ public/include/header.php
@@ -33,14 +33,22 @@
           <ul class="nav navbar-nav">          
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                    People
+                    Sellers
+                    <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a href="<?php echo BASEPATH;?>sellers">Manage Sellers</a></li>
+                    <li><a href="<?php echo BASEPATH;?>sellers/add.php">New Seller</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                    Products
                     <span class="caret"></span>
                 </a>
                 <ul class="dropdown-menu">
                     <li><a href="<?php echo BASEPATH;?>products">Manage Products</a></li>
                     <li><a href="<?php echo BASEPATH;?>products/add.php">New Product</a></li>
-                    <li><a href="<?php echo BASEPATH;?>sellers">Manage Sellers</a></li>
-                    <li><a href="<?php echo BASEPATH;?>sellers/add.php">New Seller</a></li>
                 </ul>
             </li>
           </ul>
